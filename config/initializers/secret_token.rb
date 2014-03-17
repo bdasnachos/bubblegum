5d04604f688f333af1513877630272792c4368ec
bdasnachos <bdasnachos@gmail.com>
2014-03-16 22:26:57 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bubblegum::Application.config.secret_key_base = '3b5424e0cdbb5e21608b2c4b949e120e5dd217136f39ab6a7629ca8d6459fe983eee8595b4a80e4dfca746edbb6a72ad8bef95eb9f3fa615c3831e7a078f7ca0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bubblegum::Application.config.session_store :cookie_store, key: '_bubblegum_session'
